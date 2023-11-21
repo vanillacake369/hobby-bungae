5ae345eea251ab9aaa210e31d40161a968b2c6a5
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-11-21 20:42:24 +0900

    first commit

---
+++ src/main/java/com/example/hobbybungae/HobbyBungaeApplication.java
@@ -0,0 +1,13 @@
+package com.example.hobbybungae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbyBungaeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HobbyBungaeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hobbybungae/HobbyBungaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hobbybungae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbyBungaeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
