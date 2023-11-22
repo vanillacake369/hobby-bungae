163afa952532cf513f947dbe8c457928fa218117
LeeYejin <leeyejin113@gmail.com>
2023-11-22 19:07:15 +0900

    feat: Spring security 적용

---
+++ src/main/java/com/example/hobbybungae/security/JwtAuthorizationFilter.java
@@ -0,0 +1,76 @@
+package com.example.hobbybungae.security;
+
+import com.example.hobbybungae.dto.CommonResponseDto;
+import com.example.hobbybungae.dto.ErrorResponseDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Slf4j(topic = "JWT 검증 및 인가")
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+    private final UserDetailsServiceImpl userDetailsService;
+
+    public JwtAuthorizationFilter(JwtUtil jwtUtil, UserDetailsServiceImpl userDetailsService) {
+        this.jwtUtil = jwtUtil;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(
+        HttpServletRequest req, HttpServletResponse res, FilterChain filterChain) throws ServletException, IOException {
+
+        String token = jwtUtil.getJwtFromHeader(req);
+
+        if (StringUtils.hasText(token)) {
+
+            if (!jwtUtil.validateToken(token)) {
+                // 인증정보가 존재하지 않을때
+                ObjectMapper objectMapper = new ObjectMapper();
+
+                CommonResponseDto commonResponseDto = new ErrorResponseDto("토큰이 유효하지 않습니다.");
+                res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                res.setContentType("application/json; charset=UTF-8");
+                res.getWriter().write(objectMapper.writeValueAsString(commonResponseDto));
+                return;
+            }
+
+            Claims info = jwtUtil.getUserInfoFromToken(token);
+
+            setAuthentication(info.getSubject());
+
+        }
+
+        filterChain.doFilter(req, res);
+    }
+
+    // 인증 처리
+    public void setAuthentication(String username) {
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        Authentication authentication = createAuthentication(username);
+        context.setAuthentication(authentication);
+
+        SecurityContextHolder.setContext(context);
+    }
+
+    // 인증 객체 생성
+    private Authentication createAuthentication(String username) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+    }
+
+}
+
