cabf61351d03e04f6b7460c1b3c9035f8f5f9ba2
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-11-23 06:05:56 +0900

    feat : 회원가입 요청 매핑

---
+++ src/main/java/com/example/hobbybungae/domain/user/UserController.java
@@ -0,0 +1,23 @@
+package com.example.hobbybungae.domain.user;
+
+import com.example.hobbybungae.domain.dto.CommonResponseDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/hobby-bungae/v1/users")
+public class UserController {
+
+    private final UserService userService;
+    
+    @PostMapping
+    public ResponseEntity<CommonResponseDto> signUp(@RequestBody @Valid UserRequestDto requestDto) {
+        return userService.signUp(requestDto);
+    }
+}
