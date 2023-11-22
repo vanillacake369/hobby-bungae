8fd6024b3e46021e934d0186ee7f0e18f35d26b2
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-11-22 15:18:47 +0900

    feat : user 구현

--- src/main/java/com/example/hobbybungae/domain/User.java
+++ src/main/java/com/example/hobbybungae/domain/User.java
@@ -1,4 +1,23 @@
 package com.example.hobbybungae.domain;
 
-public class User {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User extends TimeStamp{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "id_name",nullable = false)
+    private String idName;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
 }
