8180f0b74d3259d43bf28a0d02cd8397325dfbd1
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-11-23 06:06:15 +0900

    feat : 회원가입 요청 DTO 생성

--- src/main/java/com/example/hobbybungae/domain/user/UserRequestDto.java
+++ src/main/java/com/example/hobbybungae/domain/user/UserRequestDto.java
@@ -1,7 +1,16 @@
 package com.example.hobbybungae.domain.user;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
 public record UserRequestDto(
+        @NotBlank
+        @Pattern(regexp = "^[가-힣a-zA-Z]*$")
         String idName,
+        @NotBlank
+        @Pattern(regexp = "^[가-힣a-zA-Z]*$")
         String name,
+        @NotBlank
+        @Pattern(regexp = "^[가-힣a-zA-Z]*$")
         String password) {
 }
