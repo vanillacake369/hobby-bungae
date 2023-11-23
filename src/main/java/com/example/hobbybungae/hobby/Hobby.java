73e9fe6a0cc63c623ba68e215750621a0f654f32
LeeYejin <leeyejin113@gmail.com>
2023-11-23 11:30:16 +0900

    refactor: 코드 리뷰 후 수정

--- src/main/java/com/example/hobbybungae/hobby/Hobby.java
+++ src/main/java/com/example/hobbybungae/hobby/Hobby.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @Table(name = "hobbies")
 @NoArgsConstructor
+@AllArgsConstructor
 public class Hobby {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,8 +22,4 @@ public class Hobby {
 
     @Column
     private String hobbyName;
-
-    public Hobby(String hobbyName) {
-        this.hobbyName = hobbyName;
-    }
 }
