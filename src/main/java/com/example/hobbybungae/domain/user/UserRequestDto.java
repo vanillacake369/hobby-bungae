b819d5371c35c0735728b7779b2a88ea24b73627
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-11-23 16:41:38 +0900

    fix : UserResponseDto Valid 어노테이션 수정

--- src/main/java/com/example/hobbybungae/domain/user/UserRequestDto.java
+++ src/main/java/com/example/hobbybungae/domain/user/UserRequestDto.java
@@ -1,24 +1,21 @@
 package com.example.hobbybungae.domain.user;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 @Builder
 public record UserRequestDto(
         @NotBlank
         @Pattern(regexp = "^[가-힣a-zA-Z0-9]*$") // 특수문자 X
-        @Min(5)
-        @Max(16)
+        @Size(min = 5, max = 16)
         String idName,
         @NotBlank
         @Pattern(regexp = "^[가-힣a-zA-Z]*$")
         String name,
         @NotBlank
         @Pattern(regexp = "^[가-힣a-zA-Z0-9\\S]*$") // 특수문자 O
-        @Min(8)
-        @Max(32)
+        @Size(min = 8, max = 32)
         String password) {
 }
