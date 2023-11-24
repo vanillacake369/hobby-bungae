f671326aa83db846cc608ff694c94b84392895de
An <taennos1017@naver.com>
2023-11-24 22:47:43 +0900

    refactor: useProfile 조정

--- src/main/java/com/example/hobbybungae/domain/user/entity/User.java
+++ src/main/java/com/example/hobbybungae/domain/user/entity/User.java
@@ -1,6 +1,8 @@
 package com.example.hobbybungae.domain.user.entity;
 
 import com.example.hobbybungae.domain.common.TimeStamp;
+import com.example.hobbybungae.domain.hobby.entity.Hobby;
+import com.example.hobbybungae.domain.userProfile.dto.UserProfileUpdateRequestDto;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,6 +11,8 @@ import jakarta.persistence.Id;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Getter
 @Entity
@@ -24,8 +28,14 @@ public class User extends TimeStamp {
     @Column(nullable = false)
     private String name;
 
+    @Column
+    private String Introduction;
+
     @Column(nullable = false)
     private String password;
+
+    @Column
+    List<Hobby> hobbyList;
 
 //    @OneToMany
 //    List<BungaePost> bungaePosts;
@@ -35,10 +45,12 @@ public class User extends TimeStamp {
 
 
     @Builder
-    public User(Long id, String idName, String name, String password) {
+    public User(Long id, String idName, String name, String introduction, String password, List<Hobby> hobbyList) {
         this.id = id;
         this.idName = idName;
         this.name = name;
+        this.Introduction = introduction;
         this.password = password;
+        this.hobbyList = hobbyList;
     }
 }
---
+++ src/main/java/com/example/hobbybungae/domain/userProfile/exception/NotMatchingPasswordException.java
@@ -0,0 +1,4 @@
+package com.example.hobbybungae.domain.userProfile.exception;
+
+public class NotMatchingPasswordException extends RuntimeException {
+}
