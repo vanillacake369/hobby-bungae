7f79452b951c66a522faf62dbc24927fbced9654
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-11-26 04:25:31 +0900

    test: 회원가입 케이스, 중복회원 케이스 테스트

--- src/test/java/com/example/hobbybungae/domain/user/repository/UserRepositoryTest.java
+++ src/test/java/com/example/hobbybungae/domain/user/repository/UserRepositoryTest.java
@@ -9,8 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 
+@ActiveProfiles("test")
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 class UserRepositoryTest {
