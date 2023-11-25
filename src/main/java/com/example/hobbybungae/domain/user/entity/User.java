0d11f08251338c1bb5bc019caeabb244540dae59
LeeYejin <leeyejin113@gmail.com>
2023-11-25 12:26:41 +0900

    merge: 태인님 브랜치와의 충돌 해결

--- src/main/java/com/example/hobbybungae/domain/user/entity/User.java
+++ src/main/java/com/example/hobbybungae/domain/user/entity/User.java
@@ -1,30 +1,40 @@
 package com.example.hobbybungae.domain.user.entity;
 
+import com.example.hobbybungae.domain.comment.entity.Comment;
 import com.example.hobbybungae.domain.common.TimeStamp;
-import com.example.hobbybungae.domain.hobby.entity.Hobby;
-import com.example.hobbybungae.domain.userProfile.dto.UserProfileUpdateRequestDto;
+import com.example.hobbybungae.domain.post.entity.Post;
+import com.example.hobbybungae.domain.userProfile.entity.UserHobby;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 @Getter
 @Entity
 @NoArgsConstructor
 public class User extends TimeStamp {
+
+    @OneToMany(targetEntity = Post.class, mappedBy = "user")
+    List<Post> posts;
+
+    @OneToMany(targetEntity = Comment.class, mappedBy = "user")
+    List<Comment> comments;
+
+    @OneToMany(mappedBy = "user")
+    private List<UserHobby> userHobbyList = new ArrayList<>();
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @Column(nullable = false)
     private String idName;
-
     @Column(nullable = false)
     private String name;
 
@@ -34,23 +44,13 @@ public class User extends TimeStamp {
     @Column(nullable = false)
     private String password;
 
-    @Column
-    List<Hobby> hobbyList;
-
-//    @OneToMany
-//    List<BungaePost> bungaePosts;
-
-//    @OneToOne
-//    Profile profile;
-
-
     @Builder
-    public User(Long id, String idName, String name, String introduction, String password, List<Hobby> hobbyList) {
+    public User(Long id, String idName, String name, String introduction, String password, List<UserHobby> hobbyList) {
         this.id = id;
         this.idName = idName;
         this.name = name;
         this.Introduction = introduction;
         this.password = password;
-        this.hobbyList = hobbyList;
+        this.userHobbyList = hobbyList;
     }
 }
