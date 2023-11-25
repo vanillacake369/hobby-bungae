48ddc623360cbd0c9a2615e9c00d89e2cc38bc6b
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-11-26 04:24:52 +0900

    test: PostService 중복 Post 검증 테스트

--- src/test/java/com/example/hobbybungae/domain/post/service/PostServiceTest.java
+++ src/test/java/com/example/hobbybungae/domain/post/service/PostServiceTest.java
@@ -2,7 +2,7 @@ package com.example.hobbybungae.domain.post.service;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import com.example.hobbybungae.domain.common.TimeStamp;
+import com.example.hobbybungae.domain.hobby.entity.Hobby;
 import com.example.hobbybungae.domain.post.entity.Post;
 import com.example.hobbybungae.domain.post.exception.NotFoundHobbyException;
 import com.example.hobbybungae.domain.post.repository.PostRepository;
@@ -13,24 +13,31 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
-@Import({PostService.class, TimeStamp.class, PostRepository.class, Post.class})
+@Import({PostService.class, Post.class})
 class PostServiceTest {
-    @Autowired
-    private PostService postService;
-    @Autowired
-    private PostRepository postRepository;
 
-    @Test
-    @DisplayName("취미를 검증합니다.")
-    public void 취미존재여부검증() throws Exception {
-//        // GIVEN
-//        String hobby = "sdlkajlskdgjlsdakjglajk";
-//        // THEN
-//        assertThrows(NotFoundHobbyException.class, () -> {
-//            postService.validateHobbyExistence(hobby);
-//        });
-    }
+	@Autowired
+	private PostService postService;
+
+	@Autowired
+	private PostRepository postRepository;
+
+	@Test
+	@DisplayName("취미를 검증합니다.")
+	public void 취미존재여부검증() throws Exception {
+		// GIVEN
+		String hobbyName = "sdlkajlskdgjlsdakjglajk";
+		Hobby hobby = Hobby.builder()
+			.hobbyName(hobbyName)
+			.build();
+		// THEN
+		assertThrows(NotFoundHobbyException.class, () -> {
+			postService.validateHobbyExistence(hobby);
+		});
+	}
 }
