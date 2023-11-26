ad8d0a5d01a8ef923ca660ef9e62ec4402d6a8e9
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-11-26 19:03:10 +0900

    test: User에서 Hobby를 주입받아 양방향 Resolve 테스트

--- src/test/java/com/example/hobbybungae/domain/post/repository/PostHobbyRepositoryTest.java
+++ src/test/java/com/example/hobbybungae/domain/post/repository/PostHobbyRepositoryTest.java
@@ -33,12 +33,13 @@ class PostHobbyRepositoryTest {
 	HobbyRepository hobbyRepository;
 
 	@Test
-	@DisplayName("Hobby를 입력받아 PostHobby를 생성, 이에 대한 양방향 관계를 resolve합니다.")
-	public void Hobby간_양방향연관관계_해결() {
+	@DisplayName("Hobby를 입력받아 PostHobby를 생성, 이에 대한 다대다 양방향 관계를 resolve합니다.")
+	public void post_hobby_양방향연관관계_해결() {
+		// GIVEN
 		Post post = Post.builder().title("야구 번개 모아요").contents("같이 야구 하실 분 9명 모아요~").build();
 		Hobby hobby = Hobby.builder().hobbyName("야구").build();
-		// WHEN
 		hobbyRepository.save(hobby);
+		// WHEN
 		post.addHobby(hobby);
 		postRepository.save(post);
 		// THEN
