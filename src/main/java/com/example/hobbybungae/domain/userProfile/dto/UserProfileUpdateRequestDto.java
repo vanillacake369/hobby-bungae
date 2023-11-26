8fb3005f69e9117e0e76d6db3d74488d959c804f
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-11-27 02:42:14 +0900

    feat: 사용자 취미 수정 시, 기존 UserHobby 테이블 변경

--- src/main/java/com/example/hobbybungae/domain/userProfile/dto/UserProfileUpdateRequestDto.java
+++ src/main/java/com/example/hobbybungae/domain/userProfile/dto/UserProfileUpdateRequestDto.java
@@ -1,18 +1,15 @@
 package com.example.hobbybungae.domain.userProfile.dto;
 
 import com.example.hobbybungae.domain.hobby.entity.Hobby;
-import lombok.Getter;
-
 import java.util.List;
 
-@Getter
-public record UserProfileUpdateRequestDto (
-        String idName,
-        String name,
-        String passwordReconfirm,
-        String password,
-        String introduction,
-        List<Hobby> hobbyList
+public record UserProfileUpdateRequestDto(
+	String idName,
+	String name,
+	String passwordReconfirm,
+	String password,
+	String introduction,
+	List<Hobby> hobbies
 ) {
 
 }
