9ddb4ae22409166273ff79eb808cb6fffcc0a2fb
LeeYejin <leeyejin113@gmail.com>
2023-11-26 23:29:29 +0900

    feat: 취미 카테고리 삭제 기능

--- src/main/java/com/example/hobbybungae/domain/hobby/service/HobbyService.java
+++ src/main/java/com/example/hobbybungae/domain/hobby/service/HobbyService.java
@@ -4,6 +4,7 @@ import com.example.hobbybungae.domain.hobby.dto.HobbyRequestDto;
 import com.example.hobbybungae.domain.hobby.dto.HobbyResponseDto;
 import com.example.hobbybungae.domain.hobby.entity.Hobby;
 import com.example.hobbybungae.domain.hobby.exception.DuplicatedHobbyException;
+import com.example.hobbybungae.domain.hobby.exception.NotFoundHobbyException;
 import com.example.hobbybungae.domain.hobby.repository.HobbyRepository;
 import java.util.List;
 import java.util.Optional;
@@ -28,6 +29,22 @@ public class HobbyService {
         return new HobbyResponseDto(hobby);
     }
 
+    public List<HobbyResponseDto> getHobbies() {
+        return hobbyRepository.findAll().stream()
+            .map(HobbyResponseDto::new).collect(Collectors.toList());
+    }
+
+    public void deleteHobby(Long hobbyId) {
+        Hobby hobby = findHobbyEntity(hobbyId);
+        hobbyRepository.delete(hobby);
+    }
+
+    private Hobby findHobbyEntity(Long hobbyId){
+        return hobbyRepository.findById(hobbyId).orElseThrow(
+            () -> new NotFoundHobbyException("hobbyId", hobbyId.toString(), "선택한 취미 카테고리 ID가 없습니다")
+        );
+    }
+
     private void validateDuplication(String hobbyName) {
         Optional<Hobby> checkHobbyName = hobbyRepository.findByHobbyName(hobbyName);
         if (checkHobbyName.isPresent()) {
@@ -35,8 +52,9 @@ public class HobbyService {
         }
     }
 
-    public List<HobbyResponseDto> getHobbies() {
-        return hobbyRepository.findAll().stream()
-            .map(HobbyResponseDto::new).collect(Collectors.toList());
+    public void validateHobbyExistence(Hobby hobby) {
+        if (hobbyRepository.findByHobbyName(hobby.getHobbyName()).isEmpty()) {
+            throw new NotFoundHobbyException("hobby", hobby.getHobbyName(), "선택한 취미 카테고리가 없습니다");
+        }
     }
 }
